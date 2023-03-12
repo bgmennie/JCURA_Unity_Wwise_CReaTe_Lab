0fdbb41d817397de37d763d07f58f82d40189251
bgmennie <33833740+bgmennie@users.noreply.github.com>
2023-03-12 14:41:32 -0700

    Added builds folder that was originally untracked

---
+++ Builds/JCURA_Spatial_Demo_2_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,43 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_JETHRO_TULL_L = 2087463315U;
+        static const AkUniqueID PLAY_JETHRO_TULL_L_SPATIAL = 1979987118U;
+        static const AkUniqueID PLAY_JETHRO_TULL_R = 2087463309U;
+        static const AkUniqueID PLAY_JETHRO_TULL_R_SPATIAL = 3705538616U;
+        static const AkUniqueID PLAY_JETHRO_TULL_SUB = 1547780001U;
+        static const AkUniqueID PLAY_JETHRO_TULL_SUB_SPATIAL = 2451900932U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
